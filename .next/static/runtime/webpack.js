9c1b4fba25ee8c4ab16f20778a530fd973b42f3e
Leandro Vieira <leandrofernandesvieira@gmail.com>
2019-12-18 11:07:22 -0300

    feat: adjust styled components

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a45f915324d0c37fcd7e";
+/******/ 	var hotCurrentHash = "2e87ff29762622c835b0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,9 @@
 import Document, { Head, Main, NextScript } from "next/document";
-
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-
     const page = renderPage(App => props =>
       sheet.collectStyles(<App {...props} />)
     );
